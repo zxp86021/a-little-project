8345cf54c5a12490398b24ed52d850cecfba6fe0
Johnny Tsai <zxp86021@gmail.com>
2019-08-21 10:18:32 +0800

    登入找不到 email 自動註冊

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 use Socialite;
 
 class LoginController extends Controller
@@ -37,6 +38,57 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            return $this->sendLoginResponse($request);
+        } else {
+            $username = $this->username();
+            $existingUser = User::where($username, $request->{$username})->first();
+
+            if (!$existingUser) {
+                $this->validate($request, [
+                    'email' => 'required|string|email|max:255|unique:users',
+                    'password' => 'required|string|min:6',
+                ]);
+
+                $user = User::create([
+                    'name' => 'user' . rand(),
+                    'email' => $request->{$username},
+                    'password' => bcrypt($request->password),
+                ]);
+
+                $this->guard()->login($user);
+
+                return redirect($this->redirectPath());
+            }
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
     }
 
     /**
@@ -88,7 +140,7 @@ class LoginController extends Controller
                 auth()->login($newUser, true);
             }
 
-            return redirect()->to('/home');
+            return redirect()->to($this->redirectPath());
         } catch (\Exception $e) {
             return redirect('/login');
         }
