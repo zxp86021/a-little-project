f14a96900bbdf8cc123264552f320400d753e7a6
richegg <zxp86021@gmail.com>
2020-08-22 23:45:50 +0800

    星座列表改手風琴樣式

--- resources/views/astro/index.blade.php
+++ resources/views/astro/index.blade.php
@@ -18,31 +18,39 @@
                     <div class="panel-body">
                         <div class="form-horizontal">
                             @if (count($data) > 0)
-                                @foreach($data as $astro)
-                                    <div class="form-group">
-                                        <div class="col-sm-12">
-                                            <h3 class="form-control-static text-center">
-                                                {{ $astro->name }}
-                                            </h3>
-                                        </div>
-                                    </div>
-                                    @foreach(trans('astro.luck') as $index => $luckTitle)
-                                        @foreach(trans('astro.type') as $type => $typeTitle)
-                                            <div class="form-group">
-                                                <label class="col-sm-3 control-label">{{ $luckTitle }}&nbsp;{{ $typeTitle }}</label>
-                                                <div class="col-sm-9">
-                                                    <p class="form-control-static">
-                                                        @if($type === 'score')
-                                                            {!! $astroPresenter->getScoreImage( $astro->{$index . '_luck_' . $type} ) !!}
-                                                        @else
-                                                            {{ $astro->{$index . '_luck_' . $type} }}
-                                                        @endif
-                                                    </p>
+                                <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                                    @foreach($data as $astro)
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading" role="tab" id="astroHeading{{ $astro->id }}">
+                                                <h3 class="panel-title text-center">
+                                                    <a role="button" class="accordion-toggle collapsed btn-block" data-toggle="collapse" data-parent="#accordion" href="#astroCollapse{{ $astro->id }}" aria-expanded="false" aria-controls="astroCollapse{{ $astro->id }}">
+                                                        {{ $astro->name }}
+                                                    </a>
+                                                </h3>
+                                            </div>
+                                            <div id="astroCollapse{{ $astro->id }}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="astroHeading{{ $astro->id }}">
+                                                <div class="panel-body">
+                                                    @foreach(trans('astro.luck') as $index => $luckTitle)
+                                                        @foreach(trans('astro.type') as $type => $typeTitle)
+                                                            <div class="form-group">
+                                                                <label class="col-sm-3 control-label">{{ $luckTitle }}&nbsp;{{ $typeTitle }}</label>
+                                                                <div class="col-sm-9">
+                                                                    <p class="form-control-static">
+                                                                        @if($type === 'score')
+                                                                            {!! $astroPresenter->getScoreImage( $astro->{$index . '_luck_' . $type} ) !!}
+                                                                        @else
+                                                                            {{ $astro->{$index . '_luck_' . $type} }}
+                                                                        @endif
+                                                                    </p>
+                                                                </div>
+                                                            </div>
+                                                        @endforeach
+                                                    @endforeach
                                                 </div>
                                             </div>
-                                        @endforeach
+                                        </div>
                                     @endforeach
-                                @endforeach
+                                </div>
                             @else
                                 <div class="form-group">
                                     <div class="col-sm-12">
