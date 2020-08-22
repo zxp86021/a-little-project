ba7233b3063ea902e459dc2501ad5452b32565cd
richegg <zxp86021@gmail.com>
2020-08-23 00:08:48 +0800

    把註冊頁面關起來

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,7 +73,7 @@
                         <a href="{{ route('astro') }}">@lang('astro.astro')</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <!--<a href="{{ route('register') }}">Register</a>-->
                     @endauth
                 </div>
             @endif
